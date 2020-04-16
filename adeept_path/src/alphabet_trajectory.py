aadf6138c0195004ed1f5f19e35f6d757694f067
Anqi Shen <anqishen1997@gmail.com>
2020-04-16 12:42:16 -0500

    compelet letter A trajectory

--- adeept_path/src/alphabet_trajectory.py
+++ adeept_path/src/alphabet_trajectory.py
@@ -56,10 +56,12 @@ def get_alphabet_trajectory(alphabet, prev_pos=[0, 0, 0], offset=[0, 0, 0], rate
         return drawZ(prev_pos, offset)
 
 
-
 def drawA(prev_pos, offset, rate):
     t_A = 3700
-    return 0
+    waypoints = np.array([[prev_pos(0), prev_pos(1), prev_pos(2)]])
+    for t in range(0, t_A, rate):
+        waypoints = np.concatenate((waypoints, waypointA(t, prev_pos, offset)))
+    return waypoints
 
 def drawB(prev_pos, offset):
     return 0
@@ -139,40 +141,40 @@ def drawZ(prev_pos, offset):
 
 def waypointA(time, prev_xyz, offset):
     if time < 500:
-        x = prev_xyz(0) + (0 + offset(0) - prev_xyz(0)) / 500 * time
-        y = prev_xyz(1) + (0 + offset(1) - prev_xyz(1)) / 500 * time
-        z = prev_xyz(2) + (0.5 + offset(2) - prev_xyz(2)) / 500 * time
+        x = prev_xyz[0] + (0 + offset[0] - prev_xyz[0]) / 500 * time
+        y = prev_xyz[1] + (0 + offset[1] - prev_xyz[1]) / 500 * time
+        z = prev_xyz[2] + (0.5 + offset[2] - prev_xyz[2]) / 500 * time
     elif time < 550:
-        x = offset(0) + 0
-        y = offset(1) + 0
-        z = offset(2) + 0.5 + (0 - 0.5) / 50 * (time - 500)
+        x = offset[0] + 0
+        y = offset[1] + 0
+        z = offset[2] + 0.5 + (0 - 0.5) / 50 * (time - 500)
     elif time < 1550:
-        x = offset(0) + 0 + (1.5 - 0) / 1000 * (time - 550)
-        y = offset(1) + 0 + (4.0 - 0) / 1000 * (time - 550)
-        z = offset(2) + 0
+        x = offset[0] + 0 + (1.5 - 0) / 1000 * (time - 550)
+        y = offset[1] + 0 + (4.0 - 0) / 1000 * (time - 550)
+        z = offset[2] + 0
     elif time < 2550:
-        x = offset(0) + 1.5 + (3.0 - 1.5) / 1000 * (time - 1550)
-        y = offset(1) + 4.0 + (0 - 4.0) / 1000 * (time - 1550)
-        z = offset(2) + 0
+        x = offset[0] + 1.5 + (3.0 - 1.5) / 1000 * (time - 1550)
+        y = offset[1] + 4.0 + (0 - 4.0) / 1000 * (time - 1550)
+        z = offset[2] + 0
     elif time < 2600:
-        x = offset(0) + 3
-        y = offset(1) + 0
-        z = offset(2) + 0 + (0.5 - 0) / 50 * (time - 2550)
+        x = offset[0] + 3
+        y = offset[1] + 0
+        z = offset[2] + 0 + (0.5 - 0) / 50 * (time - 2550)
     elif time < 3100:
-        x = offset(0) + 3 + (0.75 - 3) / 500 * (time - 2600)
-        y = offset(1) + 0 + (2 - 0) / 500 * (time - 2600)
-        z = offset(2) + 0.5
+        x = offset[0] + 3 + (0.75 - 3) / 500 * (time - 2600)
+        y = offset[1] + 0 + (2 - 0) / 500 * (time - 2600)
+        z = offset[2] + 0.5
     elif time < 3150:
-        x = offset(0) + 0.75
-        y = offset(1) + 2
-        z = offset(2) + 0.5 + (0 - 0.5) / 50 * (time - 3100)
+        x = offset[0] + 0.75
+        y = offset[1] + 2
+        z = offset[2] + 0.5 + (0 - 0.5) / 50 * (time - 3100)
     elif time < 3650:
-        x = offset(0) + 0.75 + (2.25 - 0.75) / 500 * (time - 3150)
-        y = offset(1) + 2
-        z = offset(2) + 0
+        x = offset[0] + 0.75 + (2.25 - 0.75) / 500 * (time - 3150)
+        y = offset[1] + 2
+        z = offset[2] + 0
     elif time <= 3700:
-        x = offset(0) + 2.25
-        y = offset(1) + 2
-        z = offset(2) + 0 + (0.5 - 0) / 50 * (time - 3650)
-    return np.array([x/100, y/100, z/100])
+        x = offset[0] + 2.25
+        y = offset[1] + 2
+        z = offset[2] + 0 + (0.5 - 0) / 50 * (time - 3650)
+    return np.array([[x/100, y/100, z/100]])
 
