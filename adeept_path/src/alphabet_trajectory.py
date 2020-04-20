609f0dcb96914a68f8bc6751e9346aa723dfd306
Anqi Shen <anqishen1997@gmail.com>
2020-04-20 14:47:51 -0500

    add scaling to alphabet

--- adeept_path/src/alphabet_trajectory.py
+++ adeept_path/src/alphabet_trajectory.py
@@ -1,9 +1,18 @@
 import numpy as np
 
 
-def get_alphabet_trajectory(alphabet, prev_pos=[0, 0, 0], offset=[0, 0, 0], rate=25):
+def get_alphabet_trajectory(alphabet, prev_pos=[0, 0, 0], offset=[0, 0, 0], rate=25, scale=1):
+    '''
+    Generate the waypoints for a given character
+    :param alphabet: an English Capital letter charater
+    :param prev_pos: the previous x, y, z position of the end effector
+    :param offset: the x, y, z, formatting offset
+    :param rate: the speed of drawing, the bigger the number, fewer waypoints. (should be less than 50)
+    :param scale: to make the letter bigger (scale > 1) or smaller (0 < scale < 1)
+    :return:
+    '''
     if alphabet == 'A':
-        return drawA(prev_pos, offset, rate)
+        return drawA(prev_pos, offset, rate, scale)
     elif alphabet == 'B':
         return drawB(prev_pos, offset)
     elif alphabet == 'C':
@@ -56,11 +65,11 @@ def get_alphabet_trajectory(alphabet, prev_pos=[0, 0, 0], offset=[0, 0, 0], rate
         return drawZ(prev_pos, offset)
 
 
-def drawA(prev_pos, offset, rate):
+def drawA(prev_pos, offset, rate, scale):
     t_A = 3700
     waypoints = np.array([[prev_pos(0), prev_pos(1), prev_pos(2)]])
     for t in range(0, t_A, rate):
-        waypoints = np.concatenate((waypoints, waypointA(t, prev_pos, offset)))
+        waypoints = np.concatenate((waypoints, waypointA(t, prev_pos, offset, scale)))
     return waypoints
 
 def drawB(prev_pos, offset):
@@ -139,42 +148,42 @@ def drawZ(prev_pos, offset):
     return 0
 
 
-def waypointA(time, prev_xyz, offset):
+def waypointA(time, prev_xyz, offset, scale):
     if time < 500:
-        x = prev_xyz[0] + (0 + offset[0] - prev_xyz[0]) / 500 * time
-        y = prev_xyz[1] + (0 + offset[1] - prev_xyz[1]) / 500 * time
+        x = prev_xyz[0] + (0 + offset[0] - prev_xyz[0]) / 500 * time * scale
+        y = prev_xyz[1] + (0 + offset[1] - prev_xyz[1]) / 500 * time * scale
         z = prev_xyz[2] + (0.5 + offset[2] - prev_xyz[2]) / 500 * time
     elif time < 550:
         x = offset[0] + 0
         y = offset[1] + 0
         z = offset[2] + 0.5 + (0 - 0.5) / 50 * (time - 500)
     elif time < 1550:
-        x = offset[0] + 0 + (1.5 - 0) / 1000 * (time - 550)
-        y = offset[1] + 0 + (4.0 - 0) / 1000 * (time - 550)
+        x = offset[0] + 0 + (1.5 - 0) / 1000 * (time - 550) * scale
+        y = offset[1] + 0 + (4.0 - 0) / 1000 * (time - 550) * scale
         z = offset[2] + 0
     elif time < 2550:
-        x = offset[0] + 1.5 + (3.0 - 1.5) / 1000 * (time - 1550)
-        y = offset[1] + 4.0 + (0 - 4.0) / 1000 * (time - 1550)
+        x = offset[0] + (1.5 + (3.0 - 1.5) / 1000 * (time - 1550)) * scale
+        y = offset[1] + (4.0 + (0 - 4.0) / 1000 * (time - 1550)) * scale
         z = offset[2] + 0
     elif time < 2600:
-        x = offset[0] + 3
+        x = offset[0] + 3 * scale
         y = offset[1] + 0
         z = offset[2] + 0 + (0.5 - 0) / 50 * (time - 2550)
     elif time < 3100:
-        x = offset[0] + 3 + (0.75 - 3) / 500 * (time - 2600)
-        y = offset[1] + 0 + (2 - 0) / 500 * (time - 2600)
+        x = offset[0] + (3 + (0.75 - 3) / 500 * (time - 2600)) * scale
+        y = offset[1] + (0 + (2 - 0) / 500 * (time - 2600)) * scale
         z = offset[2] + 0.5
     elif time < 3150:
-        x = offset[0] + 0.75
-        y = offset[1] + 2
+        x = offset[0] + 0.75 * scale
+        y = offset[1] + 2 * scale
         z = offset[2] + 0.5 + (0 - 0.5) / 50 * (time - 3100)
     elif time < 3650:
-        x = offset[0] + 0.75 + (2.25 - 0.75) / 500 * (time - 3150)
-        y = offset[1] + 2
+        x = offset[0] + (0.75 + (2.25 - 0.75) / 500 * (time - 3150)) * scale
+        y = offset[1] + 2 * scale
         z = offset[2] + 0
     elif time <= 3700:
-        x = offset[0] + 2.25
-        y = offset[1] + 2
+        x = offset[0] + 2.25 * scale
+        y = offset[1] + 2 * scale
         z = offset[2] + 0 + (0.5 - 0) / 50 * (time - 3650)
     return np.array([[x/100, y/100, z/100]])
 
