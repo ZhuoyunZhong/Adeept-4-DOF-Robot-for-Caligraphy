9b04dee6868ee52269a57c1fda964f47b44249db
j-herman <61019526+j-herman@users.noreply.github.com>
2020-05-07 18:25:11 -0400

    Implemented VK

--- adeept_command/src/adeept_vk_server.py
+++ adeept_command/src/adeept_vk_server.py
@@ -2,11 +2,74 @@
 
 from adeept_command.srv import AdeeptVelFK, AdeeptVelFKResponse, AdeeptHomoMatrix, AdeeptHomoMatrixResponse, AdeeptVelIK, AdeeptVelIKResponse
 import rospy
-import numpy as np
+import numpy
 import rosservice
 from helper import ma2np
+from math import pi, atan2, sqrt
+
+def homogeneous_A(a1, alph1, d1, theta1):
+    A = numpy.array([[numpy.cos(theta1), (numpy.sin(theta1)*-1) * numpy.cos(alph1), numpy.sin(theta1) * numpy.sin(alph1), a1 * numpy.cos(theta1)],
+                      [numpy.sin(theta1), numpy.cos(theta1) * numpy.cos(alph1), (numpy.cos(theta1)*-1) * numpy.sin(alph1), a1 * numpy.sin(theta1)],
+                      [0, numpy.sin(alph1), numpy.cos(alph1), d1],
+                      [0, 0, 0, 1]])
+    return A
+
+def get_Jacobian(q1,q2,q3,q4):
+    pen = 0
+
+    #Now to find A1 A2 A3..
+    a1 = 0
+    alph1 = pi / 2
+    d1 = 0.100
+    theta1 = q1
+
+    a2 = 0.065
+    alph2 = pi
+    d2 = 0
+    theta2 = q2 + pi / 2
+
+    a3 = 0
+    alph3 = pi / 2
+    d3 = 0
+    theta3 = q3 + pi
+
+    a4 = 0
+    alph4 = 0
+    d4 = 0.055
+    theta4 = q4
+
+    a5 = 0.013+pen
+    alph5 = 0
+    d5 = 0.0665
+    theta5 = 0
+
+    A1 = homogeneous_A(a1, alph1, d1, theta1)
+    A2 = homogeneous_A(a2, alph2, d2, theta2)
+    A3 = homogeneous_A(a3, alph3, d3, theta3)
+    A4 = homogeneous_A(a4, alph4, d4, theta4)
+    A5 = homogeneous_A(a5, alph5, d5, theta5)
 
 
+
+
+    z0 = numpy.array([[0], [0], [1]])
+    z1 = A1[0:3, 2]
+    A12 = numpy.dot(A1, A2)
+    A13 = numpy.dot(A12, A3)
+    A15 = numpy.dot(A13, numpy.dot(A4, A5))
+    z2 = A12[0:3, 2]
+    z3 = A13[0:3, 2]
+    z4 = A15[0:3, 2]
+
+    o4 = A15[0:3, 3]
+    o3 = A13[0:3, 3]
+    o2 = A12[0:3, 3]
+    o1 = A1[0:3, 3]
+    Jv = numpy.column_stack((numpy.cross(z0.T, o4.T).T, numpy.cross(z1.T, (o4.T - o1.T)).T, numpy.cross(z2.T, (o4.T - o2.T)).T,
+                          numpy.cross(z3.T, (o4.T - o3.T)).T))
+    Jw = numpy.column_stack((z0, z1, z2, z3))
+    J = numpy.vstack((Jv, Jw))
+    return(J)
 
 # Handle joints velocity -> cartesian velocity...
 def handle_velocity_forward_kinematics(req):
@@ -14,40 +77,16 @@ def handle_velocity_forward_kinematics(req):
     q1_dot = req.q1_dot
     q2_dot = req.q2_dot
     q3_dot = req.q3_dot
-    qdot = np.array([[q1_dot],[q2_dot],[q3_dot]])
+    q4_dot = req.q4_dot
+    qdot = numpy.array([[q1_dot],[q2_dot],[q3_dot],[q4_dot]])
     #[q1,q2,q3] = acquire_joints()..
     q1 = req.q1
     q2 = req.q2
     q3 = req.q3
+    q4 = req.q4
 
-    #Now to find A1 A2 A3..
-    rospy.wait_for_service('homogeneous_matrix')
-    try:
-        findA = rospy.ServiceProxy('homogeneous_matrix',AdeeptHomoMatrix)
-        findAs = findA(q1,q2,q3)
-        A1 = ma2np(findAs.A1)
-        A2 = ma2np(findAs.A2)
-        A3 = ma2np(findAs.A3)
-
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-    
-    z0 = np.array([[0],[0],[1]])
-    z1 = np.array([[0],[0],[1]])
-    z2 = np.array([[0],[0],[-1]])
-
-    A13 = np.dot(A1,np.dot(A2,A3))
-    o3 = np.dot(A13,np.array([[0],[0],[0],[1]]))
-    o3 = np.delete(o3, 3,0)
-    o1 = np.dot(A1, np.array([[0], [0], [0], [1]]))
-    o1 = np.delete(o1, 3,0)
-    
-    Jv = np.column_stack((np.cross(z0.T,o3.T).T, np.cross(z1.T,(o3.T-o1.T)).T, z2))
-    Jw = np.column_stack((z0, z1, [0,0,0]))
-    J = np.vstack((Jv,Jw))
-
-    V = np.dot(J,qdot)
-    print V
+    J = get_Jacobian(q1,q2,q3,q4)
+    V = numpy.dot(J, qdot)
     Vx = V[0]
     Vy = V[1]
     Vz = V[2]
@@ -55,62 +94,38 @@ def handle_velocity_forward_kinematics(req):
     Wy = V[4]
     Wz = V[5]
 
-    print "Completed Forward Kin"
+    print "Completed Forward Vel Kinem"
     return AdeeptVelFKResponse(Vx, Vy, Vz, Wx, Wy, Wz)
 
 
 # Handle cartesian velocity -> joints velocity
 def handle_velocity_inverse_kinematics(req):
     print "Received Inverse Request"
+
     Vx = req.Vx
-    Vy = req.Vy
+    Vy = req.Vy    
     Vz = req.Vz
-    # Not using W
+
     Wx = req.Wx
     Wy = req.Wy
     Wz = req.Wz
-    
-    V = np.array([[Vx],[Vy],[Vz]])
+
+     #   V = numpy.array([[Vx], [Vy], [Vz], [Wx], [Wy], [Wz]])
+    V = numpy.array([Vx,Vy,Vz,Wx,Wy,Wz])
     q1 = req.q1
     q2 = req.q2
     q3 = req.q3
-
-    # To find A1 A2 A3
-    rospy.wait_for_service('homogeneous_matrix')
-    try:
-        findA = rospy.ServiceProxy('homogeneous_matrix', AdeeptHomoMatrix)
-        findAs = findA(q1, q2, q3)
-        A1 = ma2np(findAs.A1)
-        A2 = ma2np(findAs.A2)
-        A3 = ma2np(findAs.A3)
-
-    except rospy.ServiceException, e:
-        print "Service call failed: %s" % e
-
-    A13 = np.dot(A1,np.dot(A2,A3))
-    z0 = np.array([[0], [0], [1]])
-    z1 = np.array([[0], [0], [1]])
-    z2 = np.array([[0], [0], [-1]])
-    o3 = np.dot(A13, np.array([[0], [0], [0], [1]]))
-    o3 = np.delete(o3, 3, 0)
-    o1 = np.dot(A1, np.array([[0], [0], [0], [1]]))
-    o1 = np.delete(o1, 3, 0)
-
-    Jv = np.column_stack((np.cross(z0.T, (o3).T).T, np.cross(z1.T, (o3.T - o1.T)).T, z2))
-    #Jw = np.column_stack((z0, z1, [0, 0, 0]))
-    #J = np.vstack((Jv, Jw))
-    #JxJt = np.dot(J,J.T)..
-    #Jpinv = np.dot(np.linalg.inv(JxJt),J.T)
-    Jinv = np.linalg.inv(Jv)
-    q = np.dot(Jinv, V)
-    print q
+    q4 = req.q4
+    J = get_Jacobian(q1,q2,q3,q4)
+    Jinv = numpy.linalg.pinv(J)
+    q = numpy.dot(Jinv, V)
+    print(q)
     q1_dot = q[0]
     q2_dot = q[1]
     q3_dot = q[2]
-
+    q4_dot = q[3]
     print "Completed Inverse Kin"
-    return AdeeptVelIKResponse(True, q1_dot, q2_dot, q3_dot)
-
+    return AdeeptVelIKResponse(True, q1_dot, q2_dot, q3_dot, q4_dot)
 
 # Server
 def adeept_vk_server():
